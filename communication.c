ef9012d3c9c3e927e5c029471d474a5f931124c9
Deniz Ariyan <denizariyan@gmail.com>
2022-05-16 17:31:43 +0300

    Add basic communication with user space

---
+++ communication.c
@@ -0,0 +1,61 @@
+#include "communication.h"
+#include "messages.h"
+#include "data.h"
+
+
+NTSTATUS CreateCall(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+
+    Irp->IoStatus.Status = STATUS_SUCCESS;
+    Irp->IoStatus.Information = 0;
+
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+    
+    DebugMessage("Created Call \n");
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS CloseCall(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+
+    Irp->IoStatus.Status = STATUS_SUCCESS;
+    Irp->IoStatus.Information = 0;
+
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+    
+    DebugMessage("Closed Call \n");
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS IoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+
+    NTSTATUS Status = STATUS_UNSUCCESSFUL;
+    ULONG ByteIO = 0;
+
+    PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+
+    ULONG ControlCode = stack->Parameters.DeviceIoControl.IoControlCode;
+
+    if (ControlCode == IO_GET_CLIENT_ADDRESS)
+    {
+        PULONG Output = (PULONG)Irp->AssociatedIrp.SystemBuffer;
+        *Output = AssaultCubeBaseAddr;
+
+        DebugMessage("Client base addr requested \n");
+
+        Status = STATUS_SUCCESS;
+        ByteIO = sizeof(*Output);
+    }
+
+    Irp->IoStatus.Status = Status;
+    Irp->IoStatus.Information = ByteIO;
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+    return Status;
+}
---
+++ communication.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ntifs.h"
+
+#define IO_GET_CLIENT_ADDRESS CTL_CODE(FILE_DEVICE_UNKNOWN, 0X666, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+
+NTSTATUS IoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+
+NTSTATUS CloseCall(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+
+NTSTATUS CreateCall(PDEVICE_OBJECT DeviceObject, PIRP Irp);
